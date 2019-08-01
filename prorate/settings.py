7950f427d05f094115a204ba484aef905b6a8c3a
williammango <williammango2015@gmail.com>
2019-08-01 15:50:08 +0300

    modified settings and installed django authentication

--- prorate/settings.py
+++ prorate/settings.py
@@ -74,11 +74,12 @@ WSGI_APPLICATION = 'prorate.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prorate',
+        'USER': 'mango',
+    'PASSWORD':'mango',
     }
 }
 
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
