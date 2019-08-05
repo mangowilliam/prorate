41d9d02899bcded1de9bd8c1ca94eb2ffb390aa9
williammango <williammango2015@gmail.com>
2019-08-05 09:45:59 +0300

    added tests of the medel methods

--- prorateapp/tests.py
+++ prorateapp/tests.py
@@ -8,19 +8,52 @@ class ProfileTestclass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.mango,Profile))
     
+    def tearDown(self):
+        Profile.objects.all().delete
+    
     def test_save(self):
         self.mango.save_editor()
-        profiles = profile.objects.all()
+        profiles = Profile.objects.all()
         self.assertTrue(len(profiles)>0)
+        
+    def test_delete(self):
+        self.mango.save()
+        self.mango.delete()
+        self.assertTrue(len(Location.objects.all())== 0)
+        
+    def test_update(self):
+        self.mango>save()
+        self.mango.username="mangoes"
+        self.assertTrue(self.mango.username == 'mangoes')
+        
         
         
 class ProjectTestclass(TestCase):
     def setUp(self):
-        self.delani= Project(title = "delani", description ='landing page')
+        self.mango=Profile(username = 'mango')
+        self.mango.save_profile()
+        
+        self.new_project = Project(title = "delani", description ='landing page')
+        self.new_project.save()
+        
+    def tearDown(self):
+        Project.objects.all().delete()
+        Profile.objects.all().delete()
+        
     def test_instance(self):
+        self.delani.save()
         self.assertTrue(isinstance(self.delani,Project))
     
-    def test_save(self):
-        self.delani.save_project()
-        projects = projects.objects.all()
-        self.assertTrue(len(projects)>0)
+    def test_delete(self):
+        self.delani.save()
+        self.delani.delete()
+        self.assertTrue(len(Project.objects.all())==0)
+    
+    def test_update(self):
+        self.delani.save()
+        self.delani.title = "studio"
+        self.assertTrue(self.delani.title == "studio")
+    def test_search_project(self):
+        self.delani.save()
+        projects =Project.search_project(self.delani)
+        self.assertTrue(len(images) > 0)
